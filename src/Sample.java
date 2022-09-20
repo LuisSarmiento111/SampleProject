02d0ddb3ca39fb8907f04509eb70d91a5faba334
BT_4N2_17 <lsarmiento9311@bths.edu>
2022-09-20 11:43:03 -0400

    Added print statement for my favorite movie, "Cars"

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,7 @@
 public class Sample {
-    public static void man(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Luis Sarmiento");
+        System.out.println("Cars");
+
     }
 }
