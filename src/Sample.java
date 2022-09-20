32dff049accd41f8ae0496134eeed39fac706120
lasar <lsarmiento9311@bths.edu>
2022-09-20 19:20:06 -0400

    Added a print statement that prints my favorite food, shrimp ceviche.

--- src/Sample.java
+++ src/Sample.java
@@ -2,6 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Luis Sarmiento");
         System.out.println("Cars");
-
+        System.out.println("Shrimp Ceviche");
     }
 }
